29a301fd506a3061d4b0233f4c41080b9ccafee2
doggydigit <matthias.chinyen.tsai@gmail.com>
2022-05-06 02:50:56 +0200

    jureca test script

--- scripts/matthias/jureca_scripts/jureca_test.sh
+++ scripts/matthias/jureca_scripts/jureca_test.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-
-# Slurm job configuration
 #SBATCH --account=jinm60
 #SBATCH --nodes=1
 #SBATCH --ntasks=128
@@ -10,7 +8,15 @@
 #SBATCH --time=24:00:00
 #SBATCH --job-name=t_g_wx
 #SBATCH --partition=dc-cpu
+module purge
 source env/bin/activate
-cd BiasAdaptation/scripts/matthias/
+module load Python
+module load GCC
+module load OpenMPI
+module load mpi4py/3.1.3
+module load tqdm
+module load scikit-learn
+module load matplotlib
+cd BiasAdaptation-jureca/scripts/matthias/
 srun python3 jureca_run.py 0
 deactivate
