faa996b0e836cff333469aac330dcebe532a496e
doggydigit <matthias.chinyen.tsai@gmail.com>
2022-05-11 08:55:59 +0200

    updated script to bind job results

--- scripts/matthias/jureca_bind_jobs.py
+++ scripts/matthias/jureca_bind_jobs.py
@@ -6,16 +6,19 @@ from pickle import load as pickle_load
 if __name__ == '__main__':
     ns = [[500, 500, 500], [100, 100, 100], [25, 25, 25], [500, 500], [100, 100], [25, 25], [500], [100], [25]]
     ds = ["K49", "EMNIST_bymerge", "CIFAR100"]
-    ls = [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001]
-    gls = [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001]
-    ss = ["0,1", "2,3", "4,5", "6,7", "8,9", "10,11", "12,13", "14,15", "16,17", "18,19"]
+    ls = {"K49": ["0.0006", "0.0003", "0.0002", "0.0001", "0.00006", "0.00003", "0.00002"],
+          "EMNIST_bymerge": ["0.0006", "0.0003", "0.0002", "0.0001", "0.00006", "0.00003", "0.00002"],
+          "CIFAR100": ["0.00006", "0.00003", "0.00002", "0.00001", "0.000006", "0.000003", "0.000002"]}
+    gls = {"K49": ["0.6", "0.3", "0.2", "0.1", "0.06", "0.03", "0.02"],
+           "EMNIST_bymerge": ["0.6", "0.3", "0.2", "0.1", "0.06", "0.03", "0.02"],
+           "CIFAR100": ["0.6", "0.3", "0.2", "0.1", "0.06", "0.03", "0.02"]}
     job_text = "scan_train_xlr_wlr"
     fdir = "../../results/{}/individual/".format(job_text)
     nrsubjobs = 10
     for p1 in ns:
         for p2 in ds:
-            for p3 in ls:
-                for p4 in gls:
+            for p3 in ls[p2]:
+                for p4 in gls[p2]:
                     results = None
                     fname = "network_{}_{}_mse_tanh_es_wlr_{}_glr_{}".format(p1, p2, p3, p4)
                     for p5 in range(nrsubjobs):
@@ -27,5 +30,5 @@ if __name__ == '__main__':
                                 result = pickle_load(f)
                                 for k in []:
                                     results[k] += result[k]
-                    with open(fdir + fname + ".pickle", "wb") as f:
+                    with open(fdir + fname + "pickle", "wb") as f:
                         pickle_dump(results, f, protocol=HIGHEST_PROTOCOL)
