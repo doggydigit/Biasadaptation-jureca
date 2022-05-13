a0e106a0fab60a625eece094de4cb521b8b6e186
doggydigit <matthias.chinyen.tsai@gmail.com>
2022-05-13 11:26:06 +0200

    generalized jureca_run for more scan types

--- scripts/matthias/jureca_run.py
+++ scripts/matthias/jureca_run.py
@@ -13,20 +13,32 @@ if __name__ == '__main__':
         with open("jureca_jobs/job_{}.txt".format(jobid)) as job_file:
             jobs = job_file.readlines()
 
+        params = jobs[1 + rank].split(";")
+        if not len(params) == 5:
+            raise ValueError(params)
+        net = [list(map(int, params[0].split(',')))]
+        ds = [str(params[1])]
+        ls = [float(params[2])]
+        cl = float(params[3])
+        seeds = list(map(int, params[4].split(',')))
+        print(seeds)
+
         if "scan_train_g_xw" in jobs[0]:
-            params = jobs[1+rank].split(";")
-            if not len(params) == 5:
-                raise ValueError(params)
-            net = [list(map(int, params[0].split(',')))]
-            ds = [str(params[1])]
-            ls = [float(params[2])]
-            gl = float(params[3])
-            seeds = list(map(int, params[4].split(',')))
-            print(seeds)
             scan_params(scantype="train_g_xw", nrhiddens=net, datasets=ds, lrs=ls, early_stopping=True, recompute=False,
-                        verbose=True, saving=True, g_lr=gl, seeds=seeds)
+                        verbose=True, saving=True, g_lr=cl, seeds=seeds)
+        elif "scan_train_g_bw" in jobs[0]:
+            scan_params(scantype="train_g_bw", nrhiddens=net, datasets=ds, lrs=ls, early_stopping=True, recompute=False,
+                        verbose=True, saving=True, g_lr=cl, seeds=seeds)
+        elif "scan_train_b_w" in jobs[0]:
+            scan_params(scantype="train_b_w", nrhiddens=net, datasets=ds, lrs=ls, early_stopping=True, recompute=False,
+                        verbose=True, saving=True, b_lr=cl, seeds=seeds)
+        elif "scan_train_bmr" in jobs[0]:
+            scan_params(scantype="train_bmr", nrhiddens=net, datasets=ds, lrs=ls, early_stopping=True, recompute=False,
+                        verbose=True, saving=True, r_lr=cl, seeds=seeds)
         else:
             raise ValueError(jobs[0])
         sys.stdout.close()
+    elif len(sys.argv) == 1:
+        print(0)
     else:
         raise ValueError(sys.argv)
